e1425073c74e08c5d13566726b809135e0cbc9d0
Marshall <marshallfrink@gmail.com>
2018-02-23 16:23:14 -0800

    Did the fizzbuzz

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine((i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" : (i % 3 == 0) ? "Fizz" : (i % 5 == 0) ? "Buzz" : i.ToString());
+            }
+        }
+    }
+}
